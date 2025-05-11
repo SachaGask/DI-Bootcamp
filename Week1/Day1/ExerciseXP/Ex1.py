c0b4cea342ba6ad017616e087b16bd132d5b03e8
Sacha <sachaghnassia@gmail.com>
2025-05-11 13:52:20 +0300

    Project Bootcamp Upload

---
+++ Week1/Day1/ExerciseXP/Ex1.py
@@ -0,0 +1,73 @@
+
+# Exercise 1
+
+print("Hello world" * 4)
+
+# Exercise 2
+
+ex2 = str((99^3)*8)
+print(ex2)
+
+# Exercise 3
+
+    #>>> 5 < 3
+    #   False
+    #
+    #>>> 3 == 3
+    #   True
+    #
+    #>>> 3 == "3"
+    #   False
+    #
+    #>>> "3" > 3
+    #   False
+    #
+    #>>> "Hello" == "hello"
+    #   False
+    #
+
+# Exercise 4
+
+computer_brand = "Apple"
+print(f"I have a {computer_brand} computer.")
+
+# Exercise 5
+
+name = "Sacha"
+age = str(24)
+shoe_size = str(42)
+info = (f"My name is {name}, I am {age} years old, and my shoe size is {shoe_size}.")
+print(info)
+
+# Exercise 6
+
+a = 10
+b = 5
+
+if a>b:
+    print("Hello World.")
+
+# Exercise 7
+
+ex7 = int(input ("Choose a number: "))
+
+if ex7 %2 == 0:
+    print("This is an odd number.")
+
+# Exercise 8
+
+MYNAME = "Sacha"
+user_name = input("What is your name? ")
+
+if user_name == MYNAME:
+    print("Great bro ! Not common here")
+
+# Exercise 8
+
+height = int(input("What's your height ?"))
+
+if height > 145:
+    print("You are tall enough to ride dude !")
+
+else:
+    print("You'll need to eat some more spinach, sorry...")
