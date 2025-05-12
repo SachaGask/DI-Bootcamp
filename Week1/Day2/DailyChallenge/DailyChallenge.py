daa48c86f59476f605d2d694b2b3ee2bc14d757a
Sacha <sachaghnassia@gmail.com>
2025-05-12 16:30:40 +0300

    Day 2 Upload

---
+++ Week1/Day2/DailyChallenge/DailyChallenge.py
@@ -0,0 +1,18 @@
+
+#Ex1
+
+#input1 = int(input("Enter a number: "))  # ex: 7
+#input2 = int(input("Enter a length: "))  # ex: 5
+
+#for number in range(1, input1 * input2 + 1):
+#    if number % input1 == 0:
+#        print(number)
+
+#Ex2
+
+chaine = input("Entrez une chaine : ")
+chaine_modifiee = ""
+for i in range(len(chaine)):
+    if i == 0 or chaine[i] != chaine[i-1]:
+        chaine_modifiee += chaine[i]
+print(chaine_modifiee)
